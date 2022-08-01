884c905a9b3889896b04994977b1642afa7fa65c
Quan Hoang Nguyen <hquan@lanl.gov>
2022-08-01 17:00:30 -0600

    fix main

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from pytorch_lightning.utilities.cli import LightningCLI
 
 if __name__=='__main__':
     cli = LightningCLI(run=False, auto_registry=True, parser_kwargs={"parser_mode": "omegaconf"})
-    cli.trainer.fit(model=cli.model, datamodule=cli.datamodule, ckpt_path=cli.ckpt_path)
+    cli.trainer.fit(model=cli.model, datamodule=cli.datamodule)
     ckpt_path = cli.trainer.checkpoint_callback.last_model_path
     if ckpt_path == "":
         ckpt_path = None
