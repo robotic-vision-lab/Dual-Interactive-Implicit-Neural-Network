05322c57bbf28fab5faa6a213b7c0fd6905fa7cd
Quan Hoang Nguyen <hquan@lanl.gov>
2022-08-01 17:53:01 -0600

    debugging main

--- main.py
+++ main.py
@@ -4,3 +4,8 @@ from pytorch_lightning.utilities.cli import LightningCLI
 
 if __name__=='__main__':
     cli = LightningCLI(auto_registry=True, parser_kwargs={"parser_mode": "omegaconf"})
+    cli.trainer.fit(model=cli.model, datamodule=cli.datamodule)
+    ckpt_path = cli.trainer.checkpoint_callback.last_model_path
+    if ckpt_path == "":
+        ckpt_path = None
+    cli.trainer.test(model=cli.model, datamodule=cli.datamodule, ckpt_path=ckpt_path)
