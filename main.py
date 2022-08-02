688bc6f6ea6eb25a214c73ad65a78d16a014c517
Quan Hoang Nguyen <hquan@lanl.gov>
2022-08-01 19:04:50 -0600

    time wasted

--- main.py
+++ main.py
@@ -5,17 +5,4 @@ import torch
 from pytorch_lightning import Trainer
 
 if __name__=='__main__':
-    cli = LightningCLI(run=False, auto_registry=True, parser_kwargs={"parser_mode": "omegaconf"})
-
-    cli.trainer.fit(model=cli.model, datamodule=cli.datamodule)
-
-
-    #test
-    print("Testing!!!")
-    ckpt_path = cli.trainer.checkpoint_callback.last_model_path
-    if ckpt_path == "":
-        ckpt_path = None
-    test_config = cli.config_init.trainer
-    test_config.devices=1
-    test_trainer = Trainer(**test_config)
-    test_trainer.test(model=cli.model, datamodule=cli.datamodule, ckpt_path=ckpt_path)
+    cli = LightningCLI(auto_registry=True, parser_kwargs={"parser_mode": "omegaconf"})
