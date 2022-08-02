5aea872206efcdb045846aaa2181fede59208a41
Quan Hoang Nguyen <hquan@lanl.gov>
2022-08-02 16:32:11 -0600

    move mlp out

---
+++ src/models/components/mlp.py
@@ -0,0 +1,24 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from src.models.components.rdn import make_rdn
+from src.models.components.mlp import MLP
+import pdb
+
+class MLP(nn.Module):
+
+    def __init__(self, in_dim, out_dim, hidden_list):
+        super().__init__()
+        layers = []
+        lastv = in_dim
+        for hidden in hidden_list:
+            layers.append(nn.Linear(lastv, hidden))
+            layers.append(nn.ReLU())
+            lastv = hidden
+        layers.append(nn.Linear(lastv, out_dim))
+        self.layers = nn.Sequential(*layers)
+
+    def forward(self, x):
+        shape = x.shape[:-1]
+        x = self.layers(x.view(-1, x.shape[-1]))
+        return x.view(*shape, -1)
