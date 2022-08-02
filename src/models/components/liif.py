e692227106f84e7d43c28be98659e6c68495e0c5
Quan Hoang Nguyen <hquan@lanl.gov>
2022-08-02 13:11:18 -0600

    weird

--- src/models/components/liif.py
+++ src/models/components/liif.py
@@ -50,7 +50,13 @@ class LIIF(nn.Module):
 
     def step(self, x, split):
         _, _, _, W  = x.shape
-        return self.decoder(x.permute(0,2,3,1)).permute(0,3,1,2)
+        if split > 1:
+            out = []
+            for i in range(0, W, W//split):
+                out.append(self.decoder(x[:,:,:,i:i+W//split].permute(0,2,3,1)).permute(0,3,1,2))
+            return torch.cat(out, dim=-1)
+        else:
+            return self.decoder(x.permute(0,2,3,1)).permute(0,3,1,2)
 
     def decode(self, feat, size, split):
         #feat = self.encoder(x)
