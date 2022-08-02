2873b14be32a92ae2c12449aa04eafd42c895501
Quan Hoang Nguyen <hquan@lanl.gov>
2022-08-01 18:57:28 -0600

    debug

--- main.py
+++ main.py
@@ -2,16 +2,20 @@ from src.datamodules.sr_datamodule import *
 from src.models.sr_module import *
 from pytorch_lightning.utilities.cli import LightningCLI
 import torch
+from pytorch_lightning import Trainer
 
 if __name__=='__main__':
     cli = LightningCLI(run=False, auto_registry=True, parser_kwargs={"parser_mode": "omegaconf"})
-    #print(cli.config_init)
+
     cli.trainer.fit(model=cli.model, datamodule=cli.datamodule)
 
 
     #test
+    print("Testing!!!")
     ckpt_path = cli.trainer.checkpoint_callback.last_model_path
     if ckpt_path == "":
         ckpt_path = None
-    cli.trainer.devices=1
-    cli.trainer.test(model=cli.model, datamodule=cli.datamodule, ckpt_path=ckpt_path)
+    test_config = cli.config_init.trainer
+    test_config.devices=1
+    test_trainer = Trainer(**test_config)
+    test_trainer.test(model=cli.model, datamodule=cli.datamodule, ckpt_path=ckpt_path)
