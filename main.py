f4f31e2b06562614b6fc262d0e1e08f7b01038f4
Quan Hoang Nguyen <hquan@lanl.gov>
2022-08-01 18:35:39 -0600

    debug

--- main.py
+++ main.py
@@ -1,11 +1,16 @@
 from src.datamodules.sr_datamodule import *
 from src.models.sr_module import *
 from pytorch_lightning.utilities.cli import LightningCLI
+import torch
 
 if __name__=='__main__':
     cli = LightningCLI(run=False, auto_registry=True, parser_kwargs={"parser_mode": "omegaconf"})
     #print(cli.config_init)
     cli.trainer.fit(model=cli.model, datamodule=cli.datamodule)
+
+
+    #test
+    torch.cuda.empty_cache()
     ckpt_path = cli.trainer.checkpoint_callback.last_model_path
     if ckpt_path == "":
         ckpt_path = None
