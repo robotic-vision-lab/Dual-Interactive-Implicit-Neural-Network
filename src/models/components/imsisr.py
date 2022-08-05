d05cbfb083d6e0da4c66dbb5ce8f1fb44c17543d
Quan Hoang Nguyen <hquan@lanl.gov>
2022-08-04 21:26:44 -0600

    hot fix

--- src/models/components/imsisr.py
+++ src/models/components/imsisr.py
@@ -27,17 +27,21 @@ class SineAct(nn.Module):
 class ImplicitDecoder(nn.Module):
     def __init__(self, in_channels=64, hidden_dims=[256, 256, 256, 256], mode=1, init_q=False):
         super().__init__()
-        self.K = nn.ModuleList()
-        self.Q = nn.ModuleList()
 
         self.mode = mode
         self.init_q = init_q
 
         last_dim_K = in_channels * 9
-        last_dim_Q = in_channels * 9
+        
         if self.init_q:
             self.first_layer = nn.Sequential(nn.Conv2d(3, in_channels * 9, 1),
                                             SineAct())
+            last_dim_Q = in_channels * 9
+        else:
+            last_dim_Q = 3
+
+        self.K = nn.ModuleList()
+        self.Q = nn.ModuleList()
         if self.mode == 1:
             for hidden_dim in hidden_dims:
                 self.K.append(nn.Sequential(nn.Conv2d(last_dim_K, hidden_dim, 1),
@@ -64,7 +68,7 @@ class ImplicitDecoder(nn.Module):
                 last_dim_Q = hidden_dim
         self.last_layer = nn.Conv2d(hidden_dims[-1], 3, 1)
 
-    def _make_pos_encoding(self, x, size):
+    def _make_pos_encoding(self, x, size): 
         B, C, H, W = x.shape
         H_up, W_up = size
        
@@ -96,7 +100,7 @@ class ImplicitDecoder(nn.Module):
             return q
         elif self.mode == 2:
             k = self.K[0](x)
-            q = self.Q[0](syn_inp)
+            q = k*self.Q[0](syn_inp)
             for i in range(1, len(self.K)):
                 k = self.K[i](torch.cat([k,x], dim=1))
                 q = k*self.Q[i](q)
@@ -104,7 +108,7 @@ class ImplicitDecoder(nn.Module):
             return q
         elif self.mode == 3:
             k = self.K[0](x)
-            q = self.Q[0](syn_inp)
+            q = k*self.Q[0](syn_inp)
             for i in range(1, len(self.K)):
                 k = self.K[i](torch.cat([q,x], dim=1))
                 q = k*self.Q[i](q)
