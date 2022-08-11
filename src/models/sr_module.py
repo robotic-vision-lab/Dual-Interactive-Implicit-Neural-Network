54bcefa092f9f15601468e10856d09f04370e046
Quan Hoang Nguyen <hquan@lanl.gov>
2022-08-10 18:14:13 -0600

    hot fix

--- src/models/sr_module.py
+++ src/models/sr_module.py
@@ -11,13 +11,13 @@ def calc_psnr(sr, hr, dataset=None, scale=1, rgb_range=1):
     diff = (sr - hr) / rgb_range
     if dataset is not None:
         if dataset == 'benchmark':
-            shave = scale
+            shave = int(scale)
             if diff.size(1) > 1:
                 gray_coeffs = [65.738, 129.057, 25.064]
                 convert = diff.new_tensor(gray_coeffs).view(1, 3, 1, 1) / 256
                 diff = diff.mul(convert).sum(dim=1)
         elif dataset == 'div2k':
-            shave = scale + 6
+            shave = int(scale) + 6
         else:
             raise NotImplementedError
         valid = diff[..., shave:-shave, shave:-shave]
