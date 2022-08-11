2b9e6c5c2528e69cbf85d6cd6bef3a14ae3e0989
Quan Hoang Nguyen <hquan@lanl.gov>
2022-08-11 14:47:36 -0600

    workaround

---
+++ test.py
@@ -0,0 +1,10 @@
+from src.datamodules.sr_datamodule import *
+from src.models.sr_module import *
+from pytorch_lightning.utilities.cli import LightningCLI
+import torch
+from pytorch_lightning import Trainer
+import pdb
+
+if __name__=='__main__':
+    cli = LightningCLI(auto_registry=True, save_config_callback=None, parser_kwargs={"parser_mode": "omegaconf"})
+    #pdb.set_trace()
