c0545c806d3baab1e4519997bf8e3586ed34e885
Quan Hoang Nguyen <hquan@lanl.gov>
2022-08-12 12:45:56 -0600

    add bicubic test

--- test.py
+++ test.py
@@ -5,12 +5,16 @@ from argparse import ArgumentParser
 
 parser = ArgumentParser()
 parser.add_argument("--ckpt_path", type=str)
+parser.add_argument("--bicubic_test", action='store_true')
 args = parser.parse_args()
 
 def test(args):
-    model = SRLitModule.load_from_checkpoint(args.ckpt_path)
     datamodule = SRDataModule()
     trainer = Trainer(accelerator='gpu', devices=1)
+    if args.bicubic_test:
+        model = SRLitModule(arch='bicubic')
+    else:
+        model = SRLitModule.load_from_checkpoint(args.ckpt_path)
     trainer.test(model=model, datamodule=datamodule)
 
 if __name__=='__main__':
