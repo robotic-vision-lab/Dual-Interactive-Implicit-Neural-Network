a974285819e9e2432a9a3b75943447d2a291f813
Quan Hoang Nguyen <hquan@lanl.gov>
2022-08-18 13:03:21 -0600

    track ckpt

--- demo.py
+++ demo.py
@@ -6,6 +6,7 @@ from torchvision.io import read_image, ImageReadMode
 from torchvision.utils import save_image
 from torchvision import transforms
 from torch.nn.functional import avg_pool2d
+import torch
 import glob
 import os
 parser = ArgumentParser()
@@ -21,20 +22,23 @@ def resize_fn(img, size):
                             interpolation=transforms.InterpolationMode.BICUBIC,
                             antialias=True)(img)
 
+@torch.no_grad()
 def demo(args):
     #load image
     names_hr = sorted(glob.glob(os.path.join(str(args.hr_demo_path), '*'+args.file_ext)))
-    model = SRLitModule.load_from_checkpoint(args.ckpt_path)
+    if args.model_name == 'bicubic':
+        model = SRLitModule(arch='bicubic')
+    else:
+        model = SRLitModule.load_from_checkpoint(args.ckpt_path)
     for f_hr in names_hr:
         filename, _ = os.path.splitext(os.path.basename(f_hr))
         hr = read_image(f_hr, ImageReadMode.RGB).unsqueeze(0)/255.
-        lr_size = round(hr.shape[-2]/8), round(hr.shape[-1]/8)
+        lr_size = round(hr.shape[-2]/4), round(hr.shape[-1]/4)
         lr = resize_fn(hr, lr_size)
         save_image(lr, os.path.dirname(f_hr) + "/{}/{}_lr.png".format(args.model_name, filename))
-        
-        for s in [6, 4, 3, 2, 1]:
+        for s in [3.5, 3, 2.5, 2, 1.5, 1]:
             sr = model(lr, (round(hr.shape[-2]/s), round(hr.shape[-1]/s)))
-            save_image(avg_pool2d(sr, 3, 1, 1), os.path.dirname(f_hr) + "/{}/{}_x{}.png".format(args.model_name, filename, s))
+            save_image(sr, os.path.dirname(f_hr) + "/{}/{}_{}x{}.png".format(args.model_name, args.model_name, filename, s))
 
 
 if __name__=='__main__':
